4d4f8236813e98258f13c6981ca655dcbe94f051
abayissa <andrewbayissa@gmail.com>
2026-09-18 12:13:19 +0200

    Modification de apiIA

--- services/apiIA.js
+++ services/apiIA.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://172.20.10.5:5000";
+const BASE_URL = "http://172.20.10.3:5000";
 
 export async function getEstimation(id){
     const response = await axios.get(`${BASE_URL}/api/predict/${id}`);
